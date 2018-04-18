e4e575a4cb46da4853f52913589b17ddc76803dc
oumar <oumardiallo636@gmail.com>
2018-04-18 12:14:38 +0000

    using only walking directions

--- app.py
+++ app.py
@@ -21,11 +21,10 @@ def find_route():
 
     now = datetime.now()
 
-    stops, waypoints, status, message = trosky_api(origin, destination)
+    stops, total_path, status, message = trosky_api(origin, destination)
 
     if status == 202:
-        stops = remove_duplicate(stops)
-
+        # stops = remove_duplicate(stops)
         # chunks = [waypoints[x:x + 23] for x in range(0, len(waypoints), 23)]
 
         final_data = {"status": status,
@@ -33,23 +32,35 @@ def find_route():
                       "stops": stops,
                       "message": message}
 
-        for i in waypoints:
+        for path in total_path:
 
-            if len(i) <= 23:
-                directions_result = gmaps.directions(origin=i[0],
-                                                     destination=i[-1],
-                                                     waypoints=i,
-                                                     departure_time=now)
-                final_data["routes"].append(directions_result[0])
-            else:
-                chunks = [i[x:x + 23] for x in range(0, len(waypoints), 23)]
+            origin = path.get("origin")
+            destination = path.get("destination")
 
-                for j in chunks:
-                    directions_result = gmaps.directions(origin=j[0],
-                                                         destination=j[-1],
-                                                         waypoints=j,
-                                                         departure_time=now)
-                    final_data["routes"].append(directions_result[0])
+            directions_result = gmaps.directions(origin=origin,
+                                                 destination=destination,
+                                                 mode="walking",
+                                                 departure_time=now)
+
+            final_data["routes"].append(directions_result[0])
+
+            # if path.get("bus_name") is "Walkable":
+            #
+            #     directions_result = gmaps.directions(origin=origin,
+            #                                          destination=destination,
+            #                                          mode="walking",
+            #                                          departure_time=now)
+            #
+            #     final_data["routes"].append(directions_result[0])
+            #
+            # else:
+            #
+            #     directions_result = gmaps.directions(origin=origin,
+            #                                          destination=destination,
+            #                                          mode="transit",
+            #                                          departure_time=now)
+            #     print(directions_result)
+            #     final_data["routes"].append(directions_result[0])
 
         return jsonify(final_data)
 
@@ -91,7 +102,7 @@ def trosky_api(origin, destination):
     r = requests.get(trosky_url)
     r = r.json()
 
-    waypoints = []
+    route_path = []
     stops = []
 
     message = r.get("message")
@@ -100,6 +111,8 @@ def trosky_api(origin, destination):
     if status == 202:
 
         buses = r.get("buses")
+
+        first = True
 
         for path in r.get("paths"):
 
@@ -110,20 +123,31 @@ def trosky_api(origin, destination):
             else:
                 bus_name = buses[bus - 1].get("busName")
 
-            stops.append({"bus_name": bus_name,
-                          "stop_name": path.get("busStopsList")[0].get("busStopName"),
-                          "stop_location": path.get("busStopsList")[0].get("busStopLocation")})
+            first_stop_location = path.get("busStopsList")[0].get("busStopLocation")
+            last_stop_location = path.get("busStopsList")[-1].get("busStopLocation")
+            first_stop_name = path.get("busStopsList")[0].get("busStopName")
+            last_stop_name = path.get("busStopsList")[-1].get("busStopName")
 
-            stops.append({"bus_name": "Walkable",
-                          "stop_name": path.get("busStopsList")[-1].get("busStopName"),
-                          "stop_location": path.get("busStopsList")[-1].get("busStopLocation")})
-            temp = []
-            for bus_stop in path.get("busStopsList"):
-                temp.append(bus_stop.get("busStopLocation"))
+            single_path = {"bus_name": bus_name,
+                           "origin": first_stop_location,
+                           "destination": last_stop_location}
 
-            waypoints.append(temp)
+            route_path.append(single_path)
 
-    return stops, waypoints, status, message
+            # if this is the first iteration of the loop take the first bus stop else take the last bus stop
+            if first:
+                stop = {"bus_name": bus_name,
+                        "stop_location": first_stop_location,
+                        "stop_name": first_stop_name}
+                stops.append(stop)
+                first = False
+            else:
+                stop = {"bus_name": bus_name,
+                        "stop_location": last_stop_location,
+                        "stop_name": last_stop_name}
+                stops.append(stop)
+
+    return stops, route_path, status, message
 
 
 def remove_duplicate(raw_data):
@@ -141,25 +165,25 @@ def remove_duplicate(raw_data):
 if __name__ == "__main__":
     app.run(debug=True)
 
-# if len(i) <= 23:
-#     directions_result = gmaps.directions(origin=i[0],
-#                                          destination=i[-1],
-#                                          waypoints=i,
-#                                          departure_time=now)
-#     final_data["routes"].append(directions_result[0])
-# else:
-#     chunks = [i[x:x + 23] for x in range(0, len(waypoints), 23)]
-#
-#     for j in chunks:
-#         directions_result = gmaps.directions(origin=i[0],
-#                                              destination=i[-1],
-#                                              waypoints=i,
-#                                              departure_time=now)
-#     final_data["routes"].append(directions_result[0])
+    # if len(i) <= 23:
+    #     directions_result = gmaps.directions(origin=i[0],
+    #                                          destination=i[-1],
+    #                                          waypoints=i,
+    #                                          departure_time=now)
+    #     final_data["routes"].append(directions_result[0])
+    # else:
+    #     chunks = [i[x:x + 23] for x in range(0, len(waypoints), 23)]
+    #
+    #     for j in chunks:
+    #         directions_result = gmaps.directions(origin=i[0],
+    #                                              destination=i[-1],
+    #                                              waypoints=i,
+    #                                              departure_time=now)
+    #     final_data["routes"].append(directions_result[0])
 
-# for i in chunks:
-#     directions_result = gmaps.directions(origin=i[0],
-#                                          destination=i[-1],
-#                                          waypoints=i,
-#                                          departure_time=now)
-#     final_data["routes"].append(directions_result[0])
+    # for i in chunks:
+    #     directions_result = gmaps.directions(origin=i[0],
+    #                                          destination=i[-1],
+    #                                          waypoints=i,
+    #                                          departure_time=now)
+    #     final_data["routes"].append(directions_result[0])
